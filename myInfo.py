545ba5a436371f3ad3b6b8dbd07e459db19cb0d1
Chris-Buzz <146122780+Chris-Buzz@users.noreply.github.com>
2024-01-18 09:58:27 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+# CS175L
+# Christopher Buzaid
+# myInfo
+
+print('My name is: Christopher Buzaid')
+print('My address is: 6 Jeanne Road, Lake Grove, New York, 11755')
+print('My phone number is: 631-578-2897')
