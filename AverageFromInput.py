21934fbb18e6142334c856f8e88098ac6a4bcae9
Chris-Buzz <146122780+Chris-Buzz@users.noreply.github.com>
2024-03-19 10:03:40 -0400

    Fixed Version With num = num.strip()

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,18 +2,20 @@
 # CS 175L
 #AverageFromInput
 
+
 def main():
     numbers_file = open('numbers.txt', 'r')
     i = 0
     total = 0
     
     for num in (numbers_file):
+        num = num.strip()
         i+=1
         total +=  int(num)
         print(f'I read {i} number(s)  Current number is:   {num}  Total is:   {total}')
 
 
-    print(f' Average: {total / i}')
+    print(f'Average: {total / i}')
 
 
 if __name__ == '__main__':
