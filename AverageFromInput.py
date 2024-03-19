b919ef78ab0476959e81a6c48c2693df8f030f3b
Chris-Buzz <146122780+Chris-Buzz@users.noreply.github.com>
2024-03-19 09:44:46 -0400

    Add files via upload

---
+++ AverageFromInput.py
@@ -0,0 +1,20 @@
+#Christopher Buzaid
+# CS 175L
+#AverageFromInput
+
+def main():
+    numbers_file = open('numbers.txt', 'r')
+    i = 0
+    total = 0
+    
+    for num in (numbers_file):
+        i+=1
+        total +=  int(num)
+        print(f'I read {i} number(s)  Current number is:   {num}  Total is:   {total}')
+
+
+    print(f' Average: {total / i}')
+
+
+if __name__ == '__main__':
+    main()
